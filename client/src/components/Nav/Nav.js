acae03d81d4cd20e24b5c62a72546498a7341524
Ben Lambillotte <ben.lambillotte@gmail.com>
2018-01-15 20:20:59 -0800

    Fixed minor errors with classNames and unused code

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 const Nav = () => (
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a class="navbar-brand" href="#">CryptoBase</a>
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item active">
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a className="navbar-brand" href="#">CryptoBase</a>
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item active">
             {/* TODO Change to Link for React Routing */}
-            {/* <a class="nav-link" href="#">Home </a> */}
+            {/* <a className="nav-link" href="#">Home </a> */}
           </li>
         </ul>
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign Up</button>
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Sign Up</button>
       </div>
     </div>
   </nav>
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,15 +2,15 @@ const router = require('express').Router();
 const userController = require('../../controllers/userController');
 
 // Matches with "/api/users"
-router.route('/')
-  .get(userController.findAll)
-  .post(userController.create);
+// router.route('/')
+//   .get(userController.findAll)
+//   .post(userController.create);
 
 // Matches with "/api/users/:id"
-router
-  .route('/:id')
-  .get(userController.findById)
-  .put(userController.update)
-  .delete(userController.remove);
+// router
+//   .route('/:id')
+//   .get(userController.findById)
+//   .put(userController.update)
+//   .delete(userController.remove);
 
 module.exports = router;
