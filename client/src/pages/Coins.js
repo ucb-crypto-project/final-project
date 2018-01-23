91ac873ee449bbaa0d3613128e4fc2e071d82291
Michelle Hamdani <michellehamdani@gmail.com>
2018-01-22 22:52:46 -0800

    binance API update

--- client/src/pages/Coins.js
+++ client/src/pages/Coins.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 import Jumbotron from '../components/Jumbotron';
 import API from '../utils/API';
 import { Col, Row, Container } from '../components/Grid';
-
+// var R = require('ramda');
 
 class Coins extends React.Component {
 	// constructor method begins here:
   constructor(props) {
     super(props);
+
     this.state = {
       title: 'Binance Table',
       coins: []
@@ -16,10 +17,9 @@ class Coins extends React.Component {
   };
 
   componentDidMount() {
-    API.getCoins()
-
-  }
-
+    API.getCoins();
+//   }
+//
   render() {
     return (
       <Container fluid>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,14 @@
 import axios from "axios";
+// const binance = require('node-binance-api');
+//
+// binance.options({
+//   'APIKEY':'<api key>',
+//   'APISECRET':'<api secret>',
+//   'test':true,
+// });
 
 export default {
   getCoins: function() {
-    return axios.get("/coins");
+    return axios.get("api/coins");
   }
 };
